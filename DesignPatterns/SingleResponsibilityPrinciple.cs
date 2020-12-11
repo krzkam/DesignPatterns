2c85915df2de58edb87bdd7a2ec3279edc94514a
Krzysztof Kamiński <krzkaminski@wp.pl>
2020-12-11 13:37:17 +0100

    20201211

--- DesignPatterns/SingleResponsibilityPrinciple.cs
+++ DesignPatterns/SingleResponsibilityPrinciple.cs
@@ -1,21 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace DesignPatterns
 {
-    class SingleResponsibilityPrinciple
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento pattern
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+        }
+    }
+    //Whole point of Single Responsibility Principle is that one, typical class is responsible for one thing and has one reason to change. 
+    //Don't put all responsibility to one class
+    class SingleResponsibilityPrinciple 
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World! ");
-            Console.ReadKey();
+            var j = new Journal();
+            j.AddEntry("Entry 1");
+            j.AddEntry("Entry 2");
+            Console.WriteLine(j);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //var p = new Persistence();
+            //var filename = @"c:\temp\journal.txt";
+            //p.SaveToFile(j, filename, true);
+            //Process.Start(filename);
+
+            Console.ReadKey();
         }
     }
 }
