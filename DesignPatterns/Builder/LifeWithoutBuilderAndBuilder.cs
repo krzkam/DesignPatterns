fb05e2135462de02b341f22d0d38aef0b78e9217
KAMINKR1 <KAMINKR1@PSTM17741L>
2021-01-07 12:07:23 +0100

    20210107

---
+++ DesignPatterns/Builder/LifeWithoutBuilderAndBuilder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            if (text == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(text));
+            }
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indent * indentSize);
+            sb.AppendLine($"{i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', (indent + 1) * indentSize));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = rootName };
+        }
+    }
+
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DesignPatterns.TheSOLIDDesignPrinciples;
-
+using DesignPatterns.Builder;
 
 namespace DesignPatterns
 {
@@ -68,30 +68,59 @@ namespace DesignPatterns
             ////04 Interface Segregation Principle {
             //}
 
-            //05 Dependency Inversion Principle p1{
-            //high-level module
+            ////05 Dependency Inversion Principle p1{
+            ////high-level module
 
-            var parent = new Person { Name = "John" };
-            var child1 = new Person { Name = "Chris" };
-            var child2 = new Person { Name = "Mary" };
+            //var parent = new Person { Name = "John" };
+            //var child1 = new Person { Name = "Chris" };
+            //var child2 = new Person { Name = "Mary" };
 
-            var relationships = new Relationshps();
-            relationships.AddParentAndChild(parent, child1);
-            relationships.AddParentAndChild(parent, child2);
+            //var relationships = new Relationshps();
+            //relationships.AddParentAndChild(parent, child1);
+            //relationships.AddParentAndChild(parent, child2);
+            ////new Program(relationships);
             //new Program(relationships);
-            new Program(relationships);
+            ////}
+            // 06 Life Without Builder {            
+            //low-level builder which build the string
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
 
-
+            //more complicated:
+            var words = new[] { "hello","world"};
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                //sb.AppendFormat("<li>{0}</li>",word);
+                sb.Append("<li>"+ word + "</li>");
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
             //}
+
+            // 07 Builder {
+            // we'd rather this whole thing with html was presented in some kind three, structured format. 
+            //This is the reason that we'd like to want some kind of HTML BUILDER, so that we can build up different HTML objects with nice API
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+            //}
+
+
             Console.ReadKey();
         }
 
         ////03 Liskov Substitution Principle p2 {
         //static public int Area(Rectangle r) => r.Width * r.Height;
         ////}
-        ///
 
-        //05 Dependency Inversion Principle p2{
+        ////05 Dependency Inversion Principle p2{
         //high-level module
         //public Program(Relationshps relationshps)
         //{
@@ -105,15 +134,15 @@ namespace DesignPatterns
         //    }
         //}
 
-        //better way: we can build another constructor, but this time we don't depend on Relationships instead we depend on interface
-        public Program(IRelationshipBrowser browser)
-        {
-            foreach (var p in browser.FindAllChildrenOf("John"))
-            {
-                Console.WriteLine($"Johnk has a child called {p.Name}");
-            }
-        }
+        ////better way: we can build another constructor, but this time we don't depend on Relationships instead we depend on interface
+        //public Program(IRelationshipBrowser browser)
+        //{
+        //    foreach (var p in browser.FindAllChildrenOf("John"))
+        //    {
+        //        Console.WriteLine($"Johnk has a child called {p.Name}");
+        //    }
         //}
+        ////}
 
     }
 }
