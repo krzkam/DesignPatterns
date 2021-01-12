a2ddfa942c35b12b062067b4c37c7cb89e1af348
KAMINKR1 <KAMINKR1@PSTM17741L>
2021-01-12 14:45:29 +0100

    20210112

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -251,6 +251,25 @@ namespace DesignPatterns
             //});
             //Task.WaitAll(t1, t2);
             ////}
+            ////26 Ambient Context {
+            //var house = new Building();
+            //using (new BuildingContext(3000))
+            //{
+            //    //ground 3000
+            //    house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)));
+            //    house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
+
+            //    using (new BuildingContext(3500))
+            //    {
+            //        //1st 3500
+            //        house.Walls.Add(new Wall(new Point(0, 0), new Point(6000, 0)));
+            //        house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
+            //    }
+            //    //ground 3000
+            //    house.Walls.Add(new Wall(new Point(5000, 0), new Point(5000, 4000)));
+            //}
+            //Console.WriteLine(house);
+            ////}
             Console.ReadKey();
 
 
