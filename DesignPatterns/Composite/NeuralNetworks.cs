9ab4189856418ba3b98cc86ccb39f04eea6a5eda
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-01-15 14:54:31 +0100

    20210115

--- DesignPatterns/Composite/NeuralNetworks.cs
+++ DesignPatterns/Composite/NeuralNetworks.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DesignPatterns.Composite
 {
-    class NeuralNetworks
+    public static class ExtensionMethods
     {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+            foreach (var from in self)
+                foreach (var to in other)
+                {
+                        from.Out.Add(to);
+                        to.In.Add(from);
+                }
+    
+        }
+    }
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+
+        //public void ConnectTo(Neuron other)
+        //{
+        //    Out.Add(other);
+        //    other.In.Add(this);
+        //}
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
     }
 }
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -379,9 +379,21 @@ namespace DesignPatterns
 
             //Console.WriteLine(drawing);
             // }
-            //33 Neural Networks {
+            ////33 Neural Networks {
+            //var neuron1 = new Neuron();
+            //var neuron2 = new Neuron();
 
-            // }
+            //neuron1.ConnectTo(neuron2);
+
+            //var layer1 = new NeuronLayer();
+            //var layer2 = new NeuronLayer();
+
+            //neuron1.ConnectTo(layer1);
+            //layer1.ConnectTo(layer2);
+            //// }
+            //34 Composite Specification {
+
+            //}
             Console.ReadKey();
 
 
